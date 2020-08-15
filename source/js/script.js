d0532a66c4565c794021e29beb741cff172ebba7
githubolga <myolga17@gmail.com>
2020-08-15 22:30:59 +0300

    работа с блоком menu

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+var buttonMenu = document.querySelector(".js-menu-button");
+var menu = document.querySelector(".js-menu");
+
+if (menu.classList.contains("no-js")) {
+   menu.classList.remove("no-js");
+}
+
+
+buttonMenu.addEventListener("click", function (evt) {
+  evt.preventDefault();
+    if (menu.classList.contains("m-show")) {
+        menu.classList.remove("m-show");
+        buttonMenu.classList.remove("m-show");
+    } else {
+        menu.classList.add("m-show");
+        buttonMenu.classList.add("m-show");
+    }
+});
