7b7592813a7328bce9bd727f8e658a36f01b2ba1
githubolga <myolga17@gmail.com>
2020-09-05 00:05:06 +0300

    all blocks

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 var buttonMenu = document.querySelector(".js-menu-button");
 var menu = document.querySelector(".js-menu");
+var mapWrapper = document.querySelector(".js-map");
 
 if (menu.classList.contains("no-js")) {
   menu.classList.remove("no-js");
@@ -16,3 +17,24 @@ buttonMenu.addEventListener("click", function (evt) {
     buttonMenu.classList.add("m-show");
   }
 });
+
+ymaps.ready(map);
+
+function map () {
+  mapWrapper.classList.add("map-show");
+
+  var myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055],
+      zoom: 16,
+      controls: ["zoomControl"]
+  });
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+    hintContent: "Nerds 191186, Санкт-Петербург, ул. Б.Конюшенная, д.19/8"
+  }, {
+    iconLayout: "default#image",
+    iconImageHref: "./img/map-marker.png",
+    iconImageSize: [231, 190]
+  });
+  myMap.behaviors.disable("scrollZoom");
+  myMap.geoObjects.add(myPlacemark);
+}
